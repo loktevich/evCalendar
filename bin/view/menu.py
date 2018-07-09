886145e7ff3cf8c2f797447e517eec5f448adeb7
Dmitry Loktevich <loktevich@gmail.com>
2018-07-09 22:52:25 +0300

    little modification of menu view

--- bin/view/menu.py
+++ bin/view/menu.py
@@ -8,20 +8,20 @@ def create_menu(options):
     menu_options = []
     for option in options:
         if option == 'E':
-            menu_options.append("  [E]  View events for today")
+            menu_options.append("  [ E ]  View events for today")
         if option == 'V':
-            menu_options.append("  [V]  View calendar")
+            menu_options.append("  [ V ]  View calendar")
         if option == 'A':
-            menu_options.append("  [A]  Add a new event")
+            menu_options.append("  [ A ]  Add a new event")
         if option == 'U':
-            menu_options.append("  [U]  Update existing event")
+            menu_options.append("  [ U ]  Update existing event")
         if option == 'D':
-            menu_options.append("  [D]  Delete the event")
+            menu_options.append("  [ D ]  Delete the event")
         if option == 'F':
-            menu_options.append("  [F]  Find event(s) by date")    
+            menu_options.append("  [ F ]  Find event(s) by date")    
         if option == 'R':
-            menu_options.append("  [R]  Return to main menu")        
+            menu_options.append("  [ R ]  Return to main menu")        
         if option == 'X':
-            menu_options.append("  [X]  Exit the program")
+            menu_options.append("  [ X ]  Exit the program")
     for option in menu_options:
         print(option)
